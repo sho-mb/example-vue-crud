66e3f72949e1d4abd41f038671b9660381598e09
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-06-28 10:04:58 +0700

    feat: add ESlint and flat config formater

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>hello</h1>
   </div>
 </template>
---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import stylistic from '@stylistic/eslint-plugin'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  stylistic.configs['recommended-flat'],
+)
