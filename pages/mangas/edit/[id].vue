770afc60ad98999fe45061bdc990ff7c099482b8
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-07-08 13:07:12 +0700

    feat: add edit page

---
+++ pages/mangas/edit/[id].vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <MangasEditUpdateForm v-bind="manga" class="mt-10" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Manga } from '~/types/manga'
+
+const route = useRoute()
+const manga = ref<Manga>({
+  id: '',
+  mangaName: '',
+  author: '',
+  topViewUrl: '/image/default.jpg',
+  content: '',
+  genres: [],
+  urlOfWeb: '',
+})
+
+const { data } = await useFetch<Manga>(`/api/manga/${route.params.id}`)
+if (data.value) {
+  manga.value = {
+    id: data.value.id.toString(),
+    mangaName: data.value.mangaName,
+    author: data.value.author,
+    topViewUrl: data.value.topViewUrl,
+    content: data.value.content,
+    genres: data.value.genres,
+    urlOfWeb: data.value.urlOfWeb,
+  }
+}
+</script>
+
+<style>
+
+</style>
