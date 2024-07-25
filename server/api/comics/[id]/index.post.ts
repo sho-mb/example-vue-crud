96ebd085d082020c144712026de179a2b62c6a69
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-07-25 14:40:06 +0700

    feat: remove console log

--- server/api/comics/[id]/index.post.ts
+++ server/api/comics/[id]/index.post.ts
@@ -3,8 +3,6 @@ import { API_BASE_URL } from '~/server/config/api'
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
   const body = await readBody(event)
-  console.log(id)
-  console.log(body)
   try {
     await $fetch(`${API_BASE_URL}/comics/${id}`, {
       method: 'post',
